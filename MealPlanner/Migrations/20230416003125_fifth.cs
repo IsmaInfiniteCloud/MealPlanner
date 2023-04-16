561b341a9b6ccad6d72e68dfd6bb942f2ed20b6f
manou141 <manou141@hotmail.com>
2023-04-15 20:43:46 -0400

    added all the views for the 4 models with their controllers

---
+++ MealPlanner/Migrations/20230416003125_fifth.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MealPlanner.Migrations
+{
+    public partial class fifth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MealId",
+                table: "Recipe",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "MealPlan",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MealPlan", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Meal",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MealPlanId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meal", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Meal_MealPlan_MealPlanId",
+                        column: x => x.MealPlanId,
+                        principalTable: "MealPlan",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipe_MealId",
+                table: "Recipe",
+                column: "MealId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Meal_MealPlanId",
+                table: "Meal",
+                column: "MealPlanId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipe_Meal_MealId",
+                table: "Recipe",
+                column: "MealId",
+                principalTable: "Meal",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipe_Meal_MealId",
+                table: "Recipe");
+
+            migrationBuilder.DropTable(
+                name: "Meal");
+
+            migrationBuilder.DropTable(
+                name: "MealPlan");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recipe_MealId",
+                table: "Recipe");
+
+            migrationBuilder.DropColumn(
+                name: "MealId",
+                table: "Recipe");
+        }
+    }
+}
