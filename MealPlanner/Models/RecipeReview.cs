dba8968b177ceb13498f48a3035ba49cd78f791e
manou141 <manou141@hotmail.com>
2023-04-15 22:16:31 -0400

    added recipe review model class

---
+++ MealPlanner/Models/RecipeReview.cs
@@ -0,0 +1,17 @@
+﻿namespace MealPlanner.Models
+{
+    public class RecipeReview
+    {
+        public int Id { get; set; }
+        public int RecipeId { get; set; }
+        public string UserId { get; set; }
+        public int Rating { get; set; }
+        public string ReviewText { get; set; }
+
+        public RecipeReview()
+        {
+            
+        }
+    }
+
+}
