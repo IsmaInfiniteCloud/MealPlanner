a17b2383206356348e46db7af4cb56d51d13e6aa
manou141 <manou141@hotmail.com>
2023-04-15 20:44:51 -0400

    saved queries to add data

---
+++ SQLQuery5.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO Meal (Name, MealPlanId)
+VALUES ('Honey Garlic Chicken', 1),
+       ('Spaghetti Carbonara', 1),
+       ('Baked Salmon with Asparagus', 2),
+       ('Beef Stroganoff', 2),
+       ('Mushroom Risotto', 2);
