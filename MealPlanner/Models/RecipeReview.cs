b56fdba6507b28852460beee649798edaaffc474
manou141 <manou141@hotmail.com>
2023-04-19 20:41:54 -0400

    added silly animatiom

--- MealPlanner/Models/RecipeReview.cs
+++ MealPlanner/Models/RecipeReview.cs
@@ -1,14 +1,20 @@
-﻿namespace MealPlanner.Models
+﻿using System;
+
+namespace MealPlanner.Models
 {
     public class RecipeViewModel
     {
+        // pour accéder et définir la recette
         public Recipe Recipe { get; set; }
-        public List<RecipeReview> Reviews { get; set; }
+
+    // pour accéder et définir la liste des avis sur la recette
+    public List<RecipeReview> Reviews { get; set; }
     }
 
     public class RecipeReview
     {
         public int Id { get; set; }
+        // Identifiant unique de la recette liée à l'avis
         public int RecipeId { get; set; }
         public string UserId { get; set; }
         public int Rating { get; set; }
